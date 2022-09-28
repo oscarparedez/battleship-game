288732751d3610e8216c051b679df389af9c3433
Oscar Paredez <oaparedez17@gmail.com>
2022-09-27 21:46:13 -0600

    server fixes

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ sio = socketio.AsyncServer(cors_allowed_origins='*')
 app = web.Application()
 sio.attach(app)
 
+async def index(request):
+    with open('index.html') as f:
+        return web.Response(text=f.read(), content_type='text/html')
+
 rooms = {}
 
 @sio.event
@@ -79,6 +83,8 @@ async def chat_message(sid, data):
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
+
+app.router.add_get('/', index)
 
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
