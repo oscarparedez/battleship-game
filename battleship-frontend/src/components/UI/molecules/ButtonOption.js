dd88e0daa58f3e57b14c9353cdfede8273487471
Oscar Paredez <oaparedez17@gmail.com>
2022-09-29 19:25:03 -0600

    final styles

--- battleship-frontend/src/components/UI/molecules/ButtonOption.js
+++ battleship-frontend/src/components/UI/molecules/ButtonOption.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Title from "../atoms/Title";
+import '../uiStyles/Button.css'
 
 const ButtonOption = (props) => {
   return (
@@ -12,6 +13,7 @@ const ButtonOption = (props) => {
             room: props.room,
             username: props.username
           }}
+          className="HomeButton"
           onClick={props.renderGrid}>
             {props.buttonTitle}
         </Link>
